bc39e3a040668f444bcfd461061fc293b6264615
pedromerca-dev <pedromerca96@gmail.com>
2025-05-26 19:25:40 -0500

    Nuevo Atributo ESTADO/ Modificacion en el create-delete

--- src/Productos/domain/models/Product.js
+++ src/Productos/domain/models/Product.js
@@ -1,12 +1,13 @@
-class Product{
-    constructor({id,nombre,descripcion,cantidad,precio,id_categoria}){
+class Product {
+    constructor({ id, nombre, descripcion, cantidad, precio, id_categoria, estado }) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.cantidad = cantidad;
         this.precio = precio;
         this.id_categoria = id_categoria;
- 
+        this.estado = estado === 1; // convierte BIT a booleano
     }
 }
 module.exports = Product;
+  
--- src/Productos/infrastructure/db/ProductRepository.js
+++ src/Productos/infrastructure/db/ProductRepository.js
@@ -3,14 +3,14 @@ const Product = require('./../../domain/models/Product');
 
 class ProductRepository {
     getAll(callback) {
-        connect.query('select * from producto', (err, results) => {
+        connect.query('select * from producto WHERE ESTADO = 1', (err, results) => {
             if (err) return callback(err);
             callback(null, results.map(r => new Product(r)));
         });
     }
 
     create(product, callback) {
-        const sql = 'insert into producto (nombre,descripcion,precio,cantidad,id_categoria) values(?,?)';
+        const sql = 'INSERT INTO producto (nombre, descripcion, precio, cantidad, id_categoria, estado) VALUES (?, ?, ?, ?, ?, 1)';
         const params = [product.nombre, product.descripcion, product.precio, product.cantidad, product.id_categoria];
         connect.query(sql, params, (err, results) => {
             if (err) return callback(err);
@@ -26,7 +26,7 @@ class ProductRepository {
     }
 
     update(id, product, callback) {
-        const sql = 'update producto set nombre=?,descripcion=?,precio=?,cantidad=?,id_categoria=? where id =?';
+        const sql = 'update producto set nombre=?,descripcion=?,precio=?,cantidad=?,id_categoria=?, ESTADO where id =?';
         const params = [product.nombre, product.descripcion, product.precio, product.cantidad, product.id_categoria, id];
         connect.query(sql, params, (err, results) => {
             if (err) return callback(err);
@@ -35,7 +35,7 @@ class ProductRepository {
     }
 
     delete(id, callback) {
-        connect.query('delete from producto where id =?', [id], (err) => {
+        connect.query('UPDATE producto SET estado = 0 WHERE id = ?', [id], (err) => {
             if (err) return callback(err);
             callback(null);
         });
