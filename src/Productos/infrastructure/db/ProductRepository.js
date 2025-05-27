c4a917b2895c580f99550ce25e32ce04e73359d7
pedromerca-dev <pedromerca96@gmail.com>
2025-05-26 19:32:36 -0500

    Busqueda de ID lo hace filtrando productos borrados

--- src/Productos/infrastructure/db/ProductRepository.js
+++ src/Productos/infrastructure/db/ProductRepository.js
@@ -19,14 +19,14 @@ class ProductRepository {
     }
 
     getById(id, callback) {
-        connect.query('select * from producto where id=?', [id], (err, results) => {
+        connect.query('select * from producto where id=? and ESTADO=1', [id], (err, results) => {
             if (err) return callback(err);
             callback(null, results.map(r => new Product(r)));
         });
     }
 
     update(id, product, callback) {
-        const sql = 'update producto set nombre=?,descripcion=?,precio=?,cantidad=?,id_categoria=?, ESTADO where id =?';
+        const sql = 'update producto set nombre=?,descripcion=?,precio=?,cantidad=?,id_categoria=? where id =?';
         const params = [product.nombre, product.descripcion, product.precio, product.cantidad, product.id_categoria, id];
         connect.query(sql, params, (err, results) => {
             if (err) return callback(err);
